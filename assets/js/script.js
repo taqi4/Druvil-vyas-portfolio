d1699562101b4f856f65d171d4bf1ee910b788d1
taqi4 <hkjigar3@gmail.com>
2022-04-13 20:31:58 +0530

    inittial commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function show(){
+    var x = document.getElementById("rate");
+      x.style.display = "block";
+  
+  }
+  function hide(){
+    var x = document.getElementById("rate");
+      x.style.display = "none"; 
+  }
